716bfce251e9b8dd588ec3323aec580390222b14
Saksham Singh <Sakshamsingh14sept@gmail.com>
2026-09-19 17:19:21 +0530

    m1: add local proxy to vite for dev

--- web/vite.config.js
+++ web/vite.config.js
@@ -3,4 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://d4uzp0bsghs2h.cloudfront.net',
+        changeOrigin: true
+      }
+    }
+  }
 })
