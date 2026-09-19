9f981add0af298c56353f61d268994135f07c571
Saksham Singh <Sakshamsingh14sept@gmail.com>
2026-09-19 18:11:32 +0530

    m1: fix cloudfront distribution ID lookup in deploy script

--- scripts/deploy_web.sh
+++ scripts/deploy_web.sh
@@ -4,7 +4,8 @@ cd web || exit 1
 npm ci
 npm run build
 BUCKET_NAME=$(aws cloudformation describe-stacks --stack-name deadman-${STAGE} --query "Stacks[0].Outputs[?OutputKey=='FrontendBucketOut'].OutputValue" --output text)
-DIST_ID=$(aws cloudformation describe-stacks --stack-name deadman-${STAGE} --query "Stacks[0].Outputs[?OutputKey=='CloudFrontDomain'].OutputValue" --output text | awk -F. '{print $1}')
+DOMAIN=$(aws cloudformation describe-stacks --stack-name deadman-${STAGE} --query "Stacks[0].Outputs[?OutputKey=='CloudFrontDomain'].OutputValue" --output text)
+DIST_ID=$(aws cloudfront list-distributions --query "DistributionList.Items[?DomainName=='${DOMAIN}'].Id" --output text)
 aws s3 sync dist/ s3://${BUCKET_NAME}/ --delete
 # invalidate cloudfront
 aws cloudfront create-invalidation --distribution-id ${DIST_ID} --paths "/*"
